fbded3283f1e5b635ffe889b0cdd624fc5bfae95
anisridis <135599778+anisridis@users.noreply.github.com>
2023-08-26 07:08:08 -0700

    Update cart.js

--- assets/script/cart.js
+++ assets/script/cart.js
@@ -1,30 +1,10 @@
-const billAmt = document.querySelector('#billAmt');
-const serviceSelector = document.querySelector('#serviceSelector');
-const noOfPeople = document.querySelector('#noOfPeople');
-const calculatebtn = document.querySelector('.calculate');
+function switchForm(className, e){
+    e.preventDefault();
+    const allForm = document.querySelectorAll('form')
+    const form = document.querySelector(`form.${className}`)
 
-
-calculatebtn.addEventListener('click', function(){
-    if(billAmt.value === ""){
-        alert('Enter bill Amount Please!')
-        return false;
-    }
-        
-    if(serviceSelector.value =="0"){ 
-        alert('Choose service please');
-    return false;
+    allForm.forEach(item=>{
+        item.classList.remove('active');
+    })
+    form.classList.add('active');
 }
-
-if(noOfPeople.value === ""){
-    alert('Enter number of People sharing the bill please');
-    return false;
-}
-
-let total = (billAmt.value*serviceSelector.value)/noOfPeople.value;
-total = Math.round(total*100)/100;
-total = total.toFixed(2);
-
-
-document.querySelector('.totalTip').style.display= 'block';
-document.querySelector('.tip').textContent= total;
-})
